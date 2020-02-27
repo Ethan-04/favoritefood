c2b190a511f2529401809c05e29966a3f07f86fe
Ethan-04 <58610690+Ethan-04@users.noreply.github.com>
2020-02-27 13:25:43 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let food1 = "carbanara" 
+
+let food2 = "steak"
+
+let food3 = "ceaser salad"
+
+let food4 = "chicken"
+
+let food5 = "sushi"
+
+let listFoods = ["carbonara","steak"]
+
+game.splash("This is my first favorite food:" + listFoods[0])
